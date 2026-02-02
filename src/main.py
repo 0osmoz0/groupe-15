336d32d93a5356e6b4ae1c6aa0973eec9b3c1798
Mathis Lebel <mathislebel50@gmail.com>
2026-02-03 00:09:56 +0100

    I rewrite the colision with the sprite method

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ import pygame
 from player.player import Player
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+WIDTH, HEIGHT = 1920, 1080
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
-player1 = Player(start_pos=(100, 500), color=(255, 0, 0), controls={"left":pygame.K_a,"right":pygame.K_d,"jump":pygame.K_SPACE})
-player2 = Player(start_pos=(300, 500), color=(0, 0, 255), controls={"left":pygame.K_LEFT,"right":pygame.K_RIGHT,"jump":pygame.K_UP})
+player1 = Player(start_pos=(100, 300), color=(255, 0, 0), controls={"left":pygame.K_q,"right":pygame.K_d,"jump":pygame.K_SPACE}, screen_size = (WIDTH, HEIGHT))
+player2 = Player(start_pos=(300, 300), color=(0, 0, 255), controls={"left":pygame.K_LEFT,"right":pygame.K_RIGHT,"jump":pygame.K_UP}, screen_size = (WIDTH, HEIGHT))
 
-
+players = pygame.sprite.Group(player1, player2)
 
 running = True
 while running :
@@ -23,20 +24,15 @@ while running :
             
     screen.fill((0, 0, 0))
 
-    keys = pygame.key.get_pressed()
-    player1.handle_input(keys)
-    player1.move(screen, others=[player2])
-    player1.move(screen)
-    player1.draw(screen)
+    player1.handle_input()
+    player2.handle_input()
+    player1.update([player2])
+    player2.update([player1])
 
-
-    player2.handle_input(keys)
-    player2.move(screen, others=[player1])
-    player2.move(screen)
-    player2.draw(screen)
+    players.draw(screen)
 
     pygame.display.flip()
-    # pygame.display.update()
+
     clock.tick(60)
 
 pygame.quit()
--- src/player/player.py
+++ src/player/player.py
@@ -1,67 +1,70 @@
 import pygame
 
-class Player(object):
-    def __init__(self, start_pos, color, controls):
-        
-        self.rect = pygame.Rect(*start_pos, 50, 50)
+class Player(pygame.sprite.Sprite):
+    def __init__(self, start_pos, color, controls, screen_size):
+        super().__init__()
+        self.screen_width, self.screen_height = screen_size
         self.color = color
+
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(topleft=start_pos)
+
         self.controls = controls
-        self.speed = [0, 0] 
-        self.gravity = 0.45
+        self.speed_x = 0
+        self.speed_y = 0
+        self.gravity = 0.5
         self.move_speed = 5
         self.jump_count = 0
         self.jump_max = 2
         self.jump_force = -12
 
-    def handle_input(self, keys):
-        self.speed[0] = 0
-
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        self.speed_x = 0
         if keys[self.controls["left"]]:
-            self.speed[0] = -self.move_speed
+            self.speed_x = -self.move_speed
         if keys[self.controls["right"]]:
-            self.speed[0] = self.move_speed
-    
+            self.speed_x = self.move_speed
+
     def jump(self):
         if self.jump_count < self.jump_max:
-            if self.jump_count == 0:
-                self.speed[1] = self.jump_force
-            else:
-                self.speed[1] = self.jump_force * 1.1
+            self.speed_y = self.jump_force * (1.1 if self.jump_count > 0 else 1)
             self.jump_count += 1
-    
-    def collide_with_other(self, other):
-        if self.rect.colliderect(other.rect):
 
-            if self.speed[0] > 0: 
-                self.rect.right = other.rect.left
-            elif self.speed[0] < 0: 
-                self.rect.left = other.rect.right
-
-            if self.speed[1] > 0 : 
-                self.rect.bottom = other.rect.top
-                self.speed[1] = 0
-
-            elif self.speed [1] < 0: 
-                self.rect.top = other.rect.bottom 
-                self.speed[1] = 0
-
-    def move(self, screen, others=[]):
-        self.speed[1] += self.gravity
-
-        self.rect.x += self.speed[0]
-        self.rect.y += self.speed[1]
-
-
-        self.rect.clamp_ip(screen.get_rect())
+    def update(self, others=[]):
+        self.rect.x += self.speed_x
 
         for other in others:
-            self.collide_with_other(other)
+            if self.rect.colliderect(other.rect):
+                if self.speed_x > 0:
+                    self.rect.right = other.rect.left
+                elif self.speed_x < 0:
+                    self.rect.left = other.rect.right
 
-        if self.rect.bottom >= screen.get_height():
-            self.rect.bottom = screen.get_height()
-            self.speed[1] = 0
+
+        self.rect.left = max(self.rect.left, 0)
+        self.rect.right = min(self.rect.right, self.screen_width)
+
+
+        self.speed_y += self.gravity
+        self.rect.y += self.speed_y
+
+        for other in others:
+            if self.rect.colliderect(other.rect):
+                if self.speed_y > 0:
+                    self.rect.bottom = other.rect.top
+                    self.speed_y = 0
+                    self.jump_count = 0
+                elif self.speed_y < 0:
+                    self.rect.top = other.rect.bottom
+                    self.speed_y = 0
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.speed_y = 0
+
+        if self.rect.bottom >= self.screen_height:
+            self.rect.bottom = self.screen_height
+            self.speed_y = 0
             self.jump_count = 0
-    
-    
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
